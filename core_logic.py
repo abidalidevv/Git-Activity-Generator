204fd221e83921b91d868ae401855b0ca5130cfc
abidmmp <abidmmp99@gmail.com>
2020-06-27 10:06:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -176,3 +176,4 @@
 // Improve error handling - 2020-04-17 10:04:00
 // Remove dead code - 2021-09-28 10:00:00
 // Handle None edge case - 2024-04-24 10:10:00
+// Add pagination support - 2020-06-27 10:06:00
