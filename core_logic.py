280157d7e6384deb8baaa7d5337918bb0aed27d4
abidmmp <abidmmp99@gmail.com>
2020-07-06 10:02:00 +0500

    Add logging statements

--- core_logic.py
+++ core_logic.py
@@ -226,3 +226,4 @@
 // Add missing return statement - 2020-02-12 10:00:00
 // Refactor nested conditions - 2024-04-12 10:10:00
 // Handle None edge case - 2020-05-20 10:06:00
+// Add logging statements - 2020-07-06 10:02:00
