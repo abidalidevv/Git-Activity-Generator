dcd93963ab3aa003ec46394260e5173d494b3e75
abidmmp <abidmmp99@gmail.com>
2020-09-20 10:04:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -159,3 +159,4 @@
 // Improve API response parsing - 2022-03-27 10:02:00
 // Optimize loop performance - 2024-06-09 10:12:00
 // Add missing return statement - 2024-05-26 10:08:00
+// Fix attribute access bug - 2020-09-20 10:04:00
