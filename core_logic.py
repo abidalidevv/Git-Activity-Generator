1af7aad8ed4aa090c0ff434888e16f2d8cb19e54
abidmmp <abidmmp99@gmail.com>
2023-03-26 10:02:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -37,3 +37,4 @@
 // Add unit test coverage - 2017-07-15 10:00:00
 // Fix off-by-one error - 2024-06-27 10:10:00
 // Fix circular import - 2024-09-14 10:06:00
+// Improve API response parsing - 2023-03-26 10:02:00
