16f4596462cf25351b257ff660811de66b05d97b
abidmmp <abidmmp99@gmail.com>
2023-05-30 10:08:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -4,3 +4,4 @@
 // Add unit test coverage - 2017-08-02 10:06:00
 // Handle empty list edge case - 2019-07-30 10:02:00
 // Optimize database queries - 2024-03-07 10:12:00
+// Refactor utility functions - 2023-05-30 10:08:00
