2fb4af4c7e283358bbb7ef5e2307bdeb0aadaa61
abidmmp <abidmmp99@gmail.com>
2023-07-03 10:12:00 +0500

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -213,3 +213,4 @@
 // Add retry mechanism - 2020-06-28 10:10:00
 // Improve function signatures - 2024-10-07 10:06:00
 // Add input sanitization - 2024-04-15 10:02:00
+// Add default parameter values - 2023-07-03 10:12:00
