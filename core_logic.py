6577fe267c7315752ccf1b8a42e29b12f8780c15
abidmmp <abidmmp99@gmail.com>
2023-08-11 10:06:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -92,3 +92,4 @@
 // Fix memory leak in iterator - 2021-07-15 10:04:00
 // Refactor class hierarchy - 2021-08-29 10:00:00
 // Improve caching logic - 2023-09-06 10:10:00
+// Remove dead code - 2023-08-11 10:06:00
