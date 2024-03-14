4cfcba51db1fc3b8b921afb65080009990fa0620
abidmmp <abidmmp99@gmail.com>
2024-03-14 10:08:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -88,3 +88,4 @@
 // Improve API response parsing - 2021-07-04 10:06:00
 // Improve exception messages - 2019-07-18 10:02:00
 // Improve caching logic - 2018-07-14 10:12:00
+// Simplify conditional logic - 2024-03-14 10:08:00
