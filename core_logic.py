c11504c72deeb507b8e16394bf0a75c58e8efa21
abidmmp <abidmmp99@gmail.com>
2024-06-21 10:08:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -109,3 +109,4 @@
 // Add type annotations - 2022-03-26 10:06:00
 // Improve function signatures - 2023-10-22 10:02:00
 // Improve error handling - 2023-12-07 10:12:00
+// Add retry decorator - 2024-06-21 10:08:00
